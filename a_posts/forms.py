3277b697fe52a2c496e22f1f2f20cce6f0db36a7
Om Prakash <opytc2@gmail.com>
2024-10-31 23:18:43 +0530

    webcrawler extract img artist name from url

--- a_posts/forms.py
+++ a_posts/forms.py
@@ -2,13 +2,15 @@ from django.forms import ModelForm
 from django import forms
 from .models import *
 
+
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['url', 'body']
         labels = {
             'body' : 'Caption',
         }
         widgets = {
-            'body' : forms.Textarea(attrs={'rows':3, 'placeholder':'Add a caption ...', 'class':'font1 text-4xl'})
+            'body' : forms.Textarea(attrs={'rows':3, 'placeholder':'Add a caption ...', 'class':'font1 text-4xl'}),
+            'url' : forms.TextInput(attrs={'placeholder':'Add url ...'})
         }
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from bs4 import BeautifulSoup
+import requests
 
 def home_view(request):
     posts = Post.objects.all()
@@ -13,7 +15,24 @@ def post_create_view(request):
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            
+            website = requests.get(form.data['url'])
+            sourcecode = BeautifulSoup (website.text, 'html.parser')
+            find_image = sourcecode.select('meta[content^="https://live.staticflickr.com/"]') 
+            print(find_image)
+            image = find_image[0]['content']
+            post.image = image
+            
+            find_title = sourcecode.select('h1.photo-title')
+            title = find_title[0].text.strip()
+            post.title = title
+            
+            find_artist = sourcecode.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+            
+            post.save()
             return redirect('home')
         
         
