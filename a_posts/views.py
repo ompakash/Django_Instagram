4a8d7ded11695ccbd1d0ba962cfa90a749d7f165
Om Prakash <opytc2@gmail.com>
2024-10-31 22:49:31 +0530

    create post

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 def home_view(request):
-    return render(request, 'a_posts/home.html')
+    posts = Post.objects.all()
+    return render(request, 'a_posts/home.html', {'posts':posts})
+
+
+def post_create_view(request):
+    form = PostCreateForm()
+
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+        
+    return render(request, 'a_posts/post_create.html', {'form': form})
