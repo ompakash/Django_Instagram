7aaf8abfd7a8387fc1795c025e8284406fa55bf7
Om Prakash <opytc2@gmail.com>
2024-11-01 20:25:57 +0530

    messages after post deleted

--- a_posts/views.py
+++ a_posts/views.py
@@ -3,6 +3,7 @@ from .models import *
 from .forms import *
 from bs4 import BeautifulSoup
 import requests
+from django.contrib import messages
 
 def home_view(request):
     posts = Post.objects.all()
@@ -42,5 +43,6 @@ def post_delete_view(request, pk):
     post = Post.objects.get(id=pk)
     if request.method == 'POST':
         post.delete()
+        messages.success(request, 'Post Deleted')
         return redirect('home')
     return render(request, 'a_posts/post_delete.html',{'post':post})
