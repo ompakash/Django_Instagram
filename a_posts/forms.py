d5bb52588309e7ff11f63e4467d4493c551ba7ba
Om Prakash <opytc2@gmail.com>
2024-11-01 23:52:05 +0530

    tags features enabled

--- a_posts/forms.py
+++ a_posts/forms.py
@@ -6,23 +6,27 @@ from .models import *
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['url', 'body']
+        fields = ['url', 'body', 'tags']
         labels = {
             'body' : 'Caption',
+            'tags' : 'Category',
         }
         widgets = {
             'body' : forms.Textarea(attrs={'rows':3, 'placeholder':'Add a caption ...', 'class':'font1 text-4xl'}),
-            'url' : forms.TextInput(attrs={'placeholder':'Add url ...'})
+            'url' : forms.TextInput(attrs={'placeholder':'Add url ...'}),
+            'tags' : forms.CheckboxSelectMultiple(),
         }
         
         
 class PostEditForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['body']
+        fields = ['body', 'tags']
         labels = {
             'body' : '',
+            'tags' : 'Category'
         }
         widgets = {
-            'body' : forms.TextInput(attrs={'rows':3, 'class':'font1 text-4xl'})
+            'body' : forms.TextInput(attrs={'rows':3, 'class':'font1 text-4xl'}),
+            'tags' : forms.CheckboxSelectMultiple(),
         }
--- a_posts/views.py
+++ a_posts/views.py
@@ -34,6 +34,7 @@ def post_create_view(request):
             post.artist = artist
             
             post.save()
+            form.save_m2m()
             return redirect('home')
 
     return render(request, 'a_posts/post_create.html', {'form': form})
